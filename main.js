e09715f26653c20800812bab3e857082d77caeac
Pablo <Pablo@Pablos-MBP.local>
2025-05-14 10:36:11 +0200

    routes fixed

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-
 // Setup
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
