8a49ee6a6e8666fc77122d8b7485da2f9a31ff3b
Pablo <Pablo@Pablos-MBP.local>
2025-05-28 14:25:28 +0200

    projects images links ui

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { gsap } from "gsap";
+
 // Setup
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
