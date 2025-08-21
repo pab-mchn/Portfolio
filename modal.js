b6c49fa04776773f5f5bc06275644932f8a4c3c0
Pablo <pab.michelini@gmail.com>
2025-08-21 12:14:35 +0200

    confeti

--- modal.js
+++ modal.js
@@ -1,17 +1,53 @@
 const modal = document.getElementById('modal');
-    const modalClose = document.getElementById('modal-close');
-    const projectsNavLinks = document.querySelectorAll('a[href="#projects"]');
+const modalClose = document.getElementById('modal-close');
+const projectsNavLinks = document.querySelectorAll('a[href="#projects"]');
+const brandName  = document.getElementById('brand-name');
 
     // Abrir modal con Projects
     projectsNavLinks.forEach(link => {
       link.addEventListener('click', (e) => {
         e.preventDefault();
         modal.classList.remove('hidden');
+        var count = 200;
+        var defaults = {
+        origin: { y: 1 }
+        };
+
+      function fire(particleRatio, opts) {
+        confetti({
+          ...defaults,
+          ...opts,
+          particleCount: Math.floor(count * particleRatio)
+        });
+      }
+
+      fire(0.25, {
+        spread: 26,
+        startVelocity: 55,
       });
+      fire(0.2, {
+        spread: 60,
+      });
+      fire(0.35, {
+        spread: 100,
+        decay: 0.91,
+        scalar: 0.8
+      });
+      fire(0.1, {
+        spread: 120,
+        startVelocity: 25,
+        decay: 0.92,
+        scalar: 1.2
+      });
+      fire(0.1, {
+        spread: 120,
+        startVelocity: 45,
+      });
+            });
     });
 
 
-    // Cerrar modal
+    // Close modal
     modalClose.addEventListener('click', () => {
       modal.classList.add('hidden');
     });
@@ -19,4 +55,42 @@ const modal = document.getElementById('modal');
       if (e.target === modal) modal.classList.add('hidden');
     });
 
+    // logo snow
+    brandName.style.cursor = 'pointer';
+    brandName.addEventListener('click', () => {
+      var duration = 5 * 500;
+      var animationEnd = Date.now() + duration;
+      var skew = 1;
+
+      function randomInRange(min, max) {
+        return Math.random() * (max - min) + min;
+      }
+
+      (function frame() {
+        var timeLeft = animationEnd - Date.now();
+        var ticks = Math.max(200, 500 * (timeLeft / duration));
+        skew = Math.max(0.8, skew - 0.001);
+
+        confetti({
+          particleCount: 1,
+          startVelocity: 0,
+          ticks: ticks,
+          origin: {
+            x: Math.random(),
+            // since particles fall down, skew start toward the top
+            y: (Math.random() * skew) - 0.2
+          },
+          colors: ['#ffffff'],
+          shapes: ['circle'],
+          gravity: randomInRange(0.4, 0.6),
+          scalar: randomInRange(0.4, 1),
+          drift: randomInRange(-0.4, 0.4)
+        });
+
+        if (timeLeft > 0) {
+          requestAnimationFrame(frame);
+        }
+      }());
+    });
+    
     
